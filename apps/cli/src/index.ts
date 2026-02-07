71c68d18e916632753196836abebd4bc2b34e166
Ryan Luong <lumintriet@gmail.com>
2026-02-07 17:25:24 +0700

    feat(cli): add prime.md.

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,5 +1,11 @@
+import { join } from "node:path";
+
 let alive: boolean = true;
 let turn: number = 0;
+
+const systemPrompt = await Bun.file(
+  join(import.meta.dir, "prompts/prime.md")
+).text();
 
 while (alive) {
   console.log(`\nTurn ${turn}:`);
