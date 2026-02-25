f29d9e1bdf27e361a5e74e34ec0afda39a255f4d
Ryan Luong <lumintriet@gmail.com>
2026-02-25 20:51:36 +0700

    feat(web/ui): add ModeToggle component.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,14 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 
+import { ModeToggle } from "@/components/utils/mode-toggle";
 import { AppProviders } from "@/components/providers/app";
-import "@/globals.css";
 import { App } from "@/app";
+import "@/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProviders>
       <App />
+      <ModeToggle className="fixed top-3 right-3 z-50" />
     </AppProviders>
   </StrictMode>
 );
