3505df774df6e0db8c30dc2212bd8fdfe1b69f3d
Ryan Luong <lumintriet@gmail.com>
2026-02-26 08:53:07 +0700

    feat(grab): processFile function.

---
+++ scripts/grab/src/process-file.ts
@@ -0,0 +1,47 @@
+import type { Result } from "neverthrow";
+
+import { err, ok } from "neverthrow";
+import { consola } from "consola";
+import { parse } from "path";
+
+import { formatOutput } from "@/format-output";
+
+export async function processFile({
+  totalFiles,
+  index,
+  path,
+}: {
+  totalFiles: number;
+  index: number;
+  path: string;
+}): Promise<Result<string, unknown>> {
+  consola.start(
+    `[grab script] Processing file (${index + 1}/${totalFiles}): ${path}`
+  );
+
+  try {
+    const content = await Bun.file(path).text();
+    consola.debug(`[grab script] Got content: ${content.slice(0, 100)}...`);
+
+    const parsedFile = parse(path);
+    consola.debug(
+      `[grab script] Parse file result: ${JSON.stringify(parsedFile)}`
+    );
+
+    const formattedOutput = formatOutput({
+      ext: parsedFile.ext,
+      content,
+      path,
+    });
+    consola.debug(
+      `[grab script] Formatted content: ${formattedOutput.slice(0, 100)}...`
+    );
+
+    return ok(formattedOutput);
+  } catch (error) {
+    consola.error(`[grab script] Failed to read file: ${path}`);
+    consola.error(error);
+
+    return err(error);
+  }
+}
