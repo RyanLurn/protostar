bab56526b74fe201582994fba8d1fe049eabbe1d
Ryan Luong <lumintriet@gmail.com>
2026-02-26 20:05:24 +0700

    feat(api): configure cors.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,16 @@
+import { cors } from "hono/cors";
 import { Hono } from "hono";
 
 const app = new Hono();
 
+app.use(
+  "/*",
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Hello from Hono!");
 });
 
 export default app;
--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -3,11 +3,17 @@ import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 
 export function App() {
+  async function helloHono() {
+    const response = await fetch("http://localhost:3000/");
+    const text = await response.text();
+    toast(text);
+  }
+
   return (
     <div className="flex h-dvh flex-col items-center justify-center gap-y-4">
       <h1 className="text-3xl font-bold">Protostar</h1>
       <p className="text-lg">An autonomous AI agent</p>
-      <Button onClick={() => toast.success("You clicked!")}>Click me</Button>
+      <Button onClick={() => void helloHono()}>Hello Hono</Button>
     </div>
   );
 }
