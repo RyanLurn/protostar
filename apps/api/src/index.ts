178eeeefe8dbb42ee88bdab15d2b78cc117293f1
Ryan Luong <lumintriet@gmail.com>
2026-02-27 10:45:39 +0700

    feat(api): get count endpoint.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,4 @@
+import { getCount } from "@protostar/db";
 import { cors } from "hono/cors";
 import { Hono } from "hono";
 
@@ -10,6 +11,17 @@ const app = new Hono()
   )
   .get("/", (c) => {
     return c.text("Hello from Hono!");
+  })
+  .get("/count/:name", async (c) => {
+    const name = c.req.param("name");
+
+    const getCountResult = await getCount({ counterName: name });
+
+    if (getCountResult.isErr()) {
+      return c.json({ error: getCountResult.error.message }, 404);
+    }
+
+    return c.json({ count: getCountResult.value });
   });
 
 export default app;
