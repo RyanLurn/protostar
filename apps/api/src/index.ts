f0aa04826c593410349daf9eeb7d3930e6380dd1
Ryan Luong <lumintriet@gmail.com>
2026-02-27 10:56:38 +0700

    feat(api): increase count endpoint.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,8 @@
-import { getCount } from "@protostar/db";
+import { increaseCount, getCount } from "@protostar/db";
+import { zValidator } from "@hono/zod-validator";
 import { cors } from "hono/cors";
 import { Hono } from "hono";
+import z from "zod";
 
 const app = new Hono()
   .use(
@@ -22,6 +24,31 @@ const app = new Hono()
     }
 
     return c.json({ count: getCountResult.value });
-  });
+  })
+  .post(
+    "/counter/:name/increase",
+    zValidator(
+      "json",
+      z.object({
+        amount: z.int(),
+      })
+    ),
+    async (c) => {
+      const name = c.req.param("name");
+      const validBody = c.req.valid("json");
+
+      const increaseCountResult = await increaseCount({
+        amount: validBody.amount,
+        counterName: name,
+      });
+
+      if (increaseCountResult.isErr()) {
+        const increaseCountError = increaseCountResult.error;
+        return c.json({ error: increaseCountError.message }, 404);
+      }
+
+      return c.json({ newCount: increaseCountResult.value });
+    }
+  );
 
 export default app;
