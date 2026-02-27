8ce3f07cf71171b84b9aa3eecf758fdacf8d3fff
Ryan Luong <lumintriet@gmail.com>
2026-02-27 11:01:38 +0700

    refactor(api): replace zod default export with named z export.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import { increaseCount, getCount } from "@protostar/db";
 import { zValidator } from "@hono/zod-validator";
 import { cors } from "hono/cors";
 import { Hono } from "hono";
-import z from "zod";
+import { z } from "zod";
 
 const app = new Hono()
   .use(
