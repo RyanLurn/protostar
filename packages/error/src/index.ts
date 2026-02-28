470bbdf7179766b57093a11335b1cd9a4d0caec0
Ryan Luong <lumintriet@gmail.com>
2026-02-28 09:04:39 +0700

    feat(error): add a interface for the base error class props.

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,18 +1,28 @@
-export class ProtostarError extends Error {
+export interface ProtostarErrorProps extends ErrorOptions {
   context?: Record<string, unknown>;
   retryable?: boolean;
+  expected: boolean;
+  code: string;
+}
+
+export class ProtostarError extends Error implements ProtostarErrorProps {
+  context?: Record<string, unknown>;
+  retryable?: boolean;
+  expected: boolean;
   code: string;
 
   constructor(
     message: string,
     {
       retryable,
+      expected,
       context,
       cause,
       code,
     }: {
       context?: Record<string, unknown>;
       retryable?: boolean;
+      expected: boolean;
       cause?: unknown;
       code: string;
     }
@@ -20,6 +30,7 @@ export class ProtostarError extends Error {
     super(message, { cause });
     this.context = context;
     this.retryable = retryable;
+    this.expected = expected;
     this.code = code;
   }
 }
