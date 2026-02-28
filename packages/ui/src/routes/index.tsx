418ad9e04fac2552c74850e67aeab2ab1cf320f4
Ryan Luong <lumintriet@gmail.com>
2026-02-28 08:13:53 +0700

    feat(ui): change index page content.

--- packages/ui/src/routes/index.tsx
+++ packages/ui/src/routes/index.tsx
@@ -9,8 +9,8 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <div className="flex h-dvh flex-col items-center justify-center gap-y-4">
-      <h1 className="text-3xl font-bold">Protostar</h1>
-      <p className="text-lg">An autonomous AI agent</p>
+      <h1 className="text-3xl font-bold">Protostar UI</h1>
+      <p className="text-lg">A collection of UI components for Protostar</p>
       <Button onClick={() => toast("Hello World")}>Hello World</Button>
     </div>
   );
