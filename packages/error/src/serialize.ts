b2692c9d8b9057df26afbda4508d4c8153ed9bd9
Ryan Luong <lumintriet@gmail.com>
2026-02-28 10:37:52 +0700

    feat(error): serialize helper function.

---
+++ packages/error/src/serialize.ts
@@ -0,0 +1,25 @@
+import { serializeError } from "serialize-error";
+
+import type { ProtostarError } from "@/index";
+
+import { serializeUnknownError } from "@/unknown-error";
+
+export function serializeProtostarError(error: ProtostarError) {
+  const cause = error.cause;
+  const serializedCause =
+    cause instanceof Error
+      ? serializeError(cause)
+      : serializeUnknownError(cause);
+
+  const serializedError = {
+    retryable: error.retryable,
+    expected: error.expected,
+    message: error.message,
+    context: error.context,
+    cause: serializedCause,
+    name: error.name,
+    code: error.code,
+  };
+
+  return serializedError;
+}
