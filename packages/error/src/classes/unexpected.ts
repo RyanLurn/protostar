b516172c0081173c0bf60ed80535efea3d0d38b0
Ryan Luong <lumintriet@gmail.com>
2026-02-28 16:39:14 +0700

    feat(error): add UnexpectedError class.

---
+++ packages/error/src/classes/unexpected.ts
@@ -0,0 +1,27 @@
+import { ProtostarError } from "@/classes/base";
+
+export class ProtostarUnexpectedError extends ProtostarError {
+  declare code: "UNEXPECTED_ERROR";
+  declare retryable: false;
+  declare expected: false;
+  declare cause: Error;
+
+  constructor(
+    message: string,
+    {
+      context,
+      cause,
+    }: {
+      context?: Record<string, unknown>;
+      cause: Error;
+    }
+  ) {
+    super(message, {
+      code: "UNEXPECTED_ERROR",
+      retryable: false,
+      expected: false,
+      context,
+      cause,
+    });
+  }
+}
