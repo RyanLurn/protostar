3cc6ab464f6bfc608a708af8742e2b2272254d17
Ryan Luong <lumintriet@gmail.com>
2026-03-01 18:12:21 +0700

    fix(ui): remove script component.

--- packages/ui/src/features/theme/components/inline-script.tsx
+++
@@ -1,7 +0,0 @@
-import { ScriptOnce } from "@tanstack/react-router";
-
-import { themeScript } from "@/features/theme/script";
-
-export function InlineThemeScript() {
-  return <ScriptOnce children={themeScript} />;
-}
