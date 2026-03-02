459bb51040d18b91d755d9b3a0eed40fdb706bec
Ryan Luong <lumintriet@gmail.com>
2026-03-02 09:01:52 +0700

    feat(ui): add more components and functions to the bundle.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,25 @@
-import { Button } from "@/components/ui/button";
+export {
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuPortal,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenu,
+} from "@/components/ui/dropdown-menu";
 
-export { Button };
+export { ModeToggle } from "@/components/utils/mode-toggle";
+export { ThemeProvider } from "@/features/theme/provider";
+export { useTheme } from "@/features/theme/hook";
+export { Toaster } from "@/components/ui/sonner";
+export { Button } from "@/components/ui/button";
+export { cn } from "@/lib/utils";
+export { toast } from "sonner";
