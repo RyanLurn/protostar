be8829a90f2a6063b0e6a649edf1b640f3c2983a
Ryan Luong <lumintriet@gmail.com>
2026-03-02 08:35:55 +0700

    feat(admin): add root layout.

---
+++ apps/admin/src/routes/__root.tsx
@@ -0,0 +1,42 @@
+/// <reference types="vite/client" />
+
+import type { ReactNode } from "react";
+
+import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+import styles from "@protostar/ui/style.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        content: "width=device-width, initial-scale=1",
+        name: "viewport",
+      },
+      {
+        title: "Protostar Admin Panel",
+      },
+    ],
+    links: [
+      { rel: "stylesheet", href: styles },
+      { type: "image/svg+xml", href: "/favicon.svg", rel: "icon" },
+    ],
+  }),
+  shellComponent: RootDocument,
+});
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
