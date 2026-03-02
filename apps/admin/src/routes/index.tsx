c584ebe18ef25da7dc8cbece9b2a2d975251b1df
Ryan Luong <lumintriet@gmail.com>
2026-03-02 08:39:52 +0700

    feat(admin): add home page.

---
+++ apps/admin/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@protostar/ui";
+
+export const Route = createFileRoute("/")({ component: App });
+
+function App() {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center gap-y-2">
+      <h1 className="text-2xl font-bold">Protostar Admin Panel</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
