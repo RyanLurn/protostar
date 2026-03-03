0e1ab5eb138fad9900839bb3ed225f169cea04ed
Ryan Luong <lumintriet@gmail.com>
2026-03-03 08:00:19 +0700

    feat(server): configure port number using the constant from the common package.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,9 @@
+import { SERVER_PORT } from "@protostar/common/ports";
 import { Hono } from "hono";
 
 const app = new Hono().get("/", (c) => c.text("Hello from Protostar Server!"));
 
-export default app;
+export default {
+  port: SERVER_PORT,
+  fetch: app.fetch,
+};
